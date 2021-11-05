4c841d965ea5bed6b59234b7b743c6036a9f5b9f
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-05 13:04:11 +0100

    back to version without api fallback

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const connectHistory = require('connect-history-api-fallback');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(connectHistory());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
